157846256e2a688700540061b223554a859a886a
B00156641 Neimanas Sarmukas <b00156641@mytudublin.ie>
2025-04-10 15:11:17 +0100

    'ports' page showing up

--- resources/views/ports/table.blade.php
+++ resources/views/ports/table.blade.php
@@ -10,11 +10,11 @@
             <td>{!! $port->port_name !!}</td>
             <td>{!! $port->port_country !!}</td>
             <td>
-                {!! Form::open(['route' => ['ports.destroy', $port->id], 'method' => 'delete']) !!}
+                {!! Form::open(['route' => ['ports.destroy', $port->port_id], 'method' => 'delete']) !!} <!-- Changed from $port->id to $port->port_id -->
                 <div class='btn-group'>
-                    <a href="{!! route('ports.show', [$port->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></i></a>
-                    <a href="{!! route('ports.edit', [$port->id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></i></a>
-                    {!! Form::button('<i class="far fa-trash-alt"></i></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    <a href="{!! route('ports.show', [$port->port_id]) !!}" class='btn btn-default btn-xs'><i class="far fa-eye"></i></a>
+                    <a href="{!! route('ports.edit', [$port->port_id]) !!}" class='btn btn-default btn-xs'><i class="far fa-edit"></i></a>
+                    {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 </div>
                 {!! Form::close() !!}
             </td>
