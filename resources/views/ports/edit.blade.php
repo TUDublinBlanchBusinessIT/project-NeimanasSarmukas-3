f7537831b38206a2d46d956bf8708efec3322e58
B00156641 Neimanas Sarmukas <b00156641@mytudublin.ie>
2025-04-16 14:56:02 +0100

    ports edit working

--- resources/views/ports/edit.blade.php
+++ resources/views/ports/edit.blade.php
@@ -11,7 +11,7 @@
        <div class="box box-primary">
            <div class="box-body">
                <div class="row">
-                   {!! Form::model($port, ['route' => ['ports.update', $port->id], 'method' => 'patch']) !!}
+                   {!! Form::model($port, ['route' => ['ports.update', $port->port_id], 'method' => 'patch']) !!}
 
                         @include('ports.fields')
 
